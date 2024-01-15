c1fc04e784e834fc28d178ab6772cbbe3f394bf4
sumairq <sumairqaisar992@gmail.com>
2024-01-15 19:06:20 +0500

    Implement "what we do " section

--- src/components/WhatWeDo/WhatWeDo.jsx
+++ src/components/WhatWeDo/WhatWeDo.jsx
@@ -1,15 +1,71 @@
 import React from 'react';
 import './WhatWeDo.css';
+import { features } from '@/src/utils/data';
+import Image from 'next/image';
 
 const WhatWeDo = () => {
   return (
     <div className='wwd-wrapper'>
         <div className='container'>
             <div className='wwd-container'> 
+            {/* Head of the section */}
                 <div className='wwd-head'>  
                     <span className='tag'>What We Do</span>
                     <span className='title'>{" "}We enable constant enterprise transformation at speed and scale</span>
                     <span className='des'>Immediate and 100% Guaranteed</span>
+                </div>
+
+                {/* Below Two Section Blocks */}
+                <div className='wwd-blocks'>
+                    {/* First Block */}
+                    <div className='wwd-block'>
+                        <span className='sec-title'>Revenue</span>
+                        <span className='text'>Web & Digital Marketing</span>    
+
+                        <div className='block-features'>
+                            {
+                               features.slice(0,3).map((feature, i ) => (
+                                <div className='block-feature' key={i}>
+                                    <Image
+                                    src={feature.icon}
+                                    alt='feature'
+                                    width={60}
+                                    height={60}
+                                    />
+                                    <span>{feature.title}</span>
+                                </div>
+                               ))
+                            
+                            }
+                        </div>
+                    </div>
+
+                    {/* Second Block */}
+                    <div className='wwd-block'>
+                        <span className='sec-title'>Repayment</span>
+                        <span className='text'>We create and manage your google and digital marketing</span>    
+
+                        <div className='block-features'>
+                            {
+                               features.slice(3,6).map((feature, i ) => (
+                                <div className='block-feature' key={i}>
+                                    <Image
+                                    src={feature.icon}
+                                    alt='feature'
+                                    width={60}
+                                    height={60}
+                                    />
+                                    <span>{feature.title}</span>
+                                </div>
+                               ))
+                            
+                            }
+                        </div>
+                    </div>
+
+                    {/* Support Block */}
+
+                  
                 </div>
             </div>
         </div>
--- src/utils/data.js
+++ src/utils/data.js
@@ -31,3 +31,30 @@ export const HeroData = [
         delay: 0.25, 
     }
 ]
+
+export const features = [
+    {
+        icon: "/features/Frame-0.png",
+        title: "3x monthly revenue",
+    },
+    {
+        icon: "/features/Frame-1.png",
+        title: "Flat fee of 8-16%",
+    },
+    {
+        icon: "/features/Frame-2.png",
+        title: "Flexible repayments by revenue shares",
+    },
+    {
+        icon: "/features/Frame-3.png",
+        title: "6-8 years repayment",
+    },
+    {
+        icon: "/features/Frame-4.png",
+        title: "$15-150K seed investment",
+    },
+    {
+        icon: "/features/Frame-5.png",
+        title: "4% of revenue upto 4X cap",
+    },
+]

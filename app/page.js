ae0c875bae985cce0e3a6553ea6b32b728e6d9e9
sumairq <sumairqaisar992@gmail.com>
2024-01-15 17:08:09 +0500

    Animate branding video transitions

--- app/page.js
+++ app/page.js
@@ -1,12 +1,16 @@
 import Navbar from '@/src/components/Navbar/Navbar'
 import Hero from '@/src/components/Hero/Hero'
 import './page.css'
+import BrandingVideo from '@/src/components/BrandingVideo/BrandingVideo'
+import WhatWeDo from '@/src/components/WhatWeDo/WhatWeDo'
 
 export default function Home() {
   return (
     <div className='app'>
       <Navbar/>
       <Hero/>
+      <BrandingVideo/>
+      <WhatWeDo/>
     </div>
   )
 }
---
+++ src/components/WhatWeDo/WhatWeDo.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import './WhatWeDo.css';
+
+const WhatWeDo = () => {
+  return (
+    <div className='wwd-wrapper'>
+        <div className='container'>
+            <div className='wwd-container'> 
+                <div className='wwd-head'>  
+                    <span className='tag'>What We Do</span>
+                    <span className='title'>{" "}We enable constant enterprise transformation at speed and scale</span>
+                    <span className='des'>Immediate and 100% Guaranteed</span>
+                </div>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default WhatWeDo

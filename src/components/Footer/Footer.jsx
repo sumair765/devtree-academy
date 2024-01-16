8989b819a672b5adb10f2541821680e7f7e075a3
sumairq <sumairqaisar992@gmail.com>
2024-01-16 16:53:56 +0500

    Add smooth scroll links to footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import './Footer.css'
 import EmailBox from '../EmailBox/EmailBox'
+import { Link } from 'react-scroll'
 
 
 const Footer = () => {
@@ -13,11 +14,21 @@ const Footer = () => {
 
                 <hr/>
                 <div className='f-menu'>
-                    <span>Home</span>
-                    <span>What we do</span>
-                    <span>How it works</span>
-                    <span>Who we invest in</span>
-                    <span>Testimonials</span>
+                <Link to="h-wrapper" spy={true} smooth={true}>
+                <span>Home</span>
+              </Link>
+                    <Link to="wwd-wrapper" spy={true} smooth={true}>
+                <span>What we do</span>
+              </Link>
+              <Link to="hiw-wrapper" spy smooth offset={100}>
+                <span>How it works</span>
+              </Link>
+              <Link to="wwi-wrapper" spy smooth>
+                <span>Who we invest in</span>
+              </Link>
+              <Link to="t-wrapper" spy smooth offset={100}>
+                <span>Testimonials</span>
+              </Link>
                 </div>
 
                 <hr/>
